4feec33001f49a3219de2624c81302b337bdf6bb
Adi-112 <adp6943@gmail.com>
2026-02-08 21:26:11 +0530

    solved this using binary search

---
+++ leets/leet374.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, h = n;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            int res = guess(mid);
+
+            if (res == 0) {
+                return mid;
+            } else if (res < 0) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
