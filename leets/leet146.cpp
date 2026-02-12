2fb13649fe493892fda472822fd36fda693f0262
Adi-112 <adp6943@gmail.com>
2026-02-12 11:42:24 +0530

    lru cache 24/24

--- leets/leet146.cpp
+++ leets/leet146.cpp
@@ -1,75 +1,4 @@
-// // /**
-// //  * Your LRUCache object will be instantiated and called as such:
-// //  * LRUCache* obj = new LRUCache(capacity);
-// //  * int param_1 = obj->get(key);
-// //  * obj->put(key,value);
-// //  */
 
-// class LRUCache {
-//     int cap;
-//     struct Node {
-//         int key, val;
-//         Node *prev, *next;
-//         Node(int _key, int _val) : key(_key), val(_val), prev(nullptr), next(nullptr){}
-//     };
-    
-//     unordered_map<int, Node*> mp;
-//     Node *head, *tail;
-// // rm. a node form ddl
-//     void remove(Node* node){
-//         node->prev->next = node->next;
-//         node->next->prev = node->prev;
-//     }
-//     void insert(Node* node){
-//         node->next = head->next;
-//         node->next->prev = node;
-//         head->next = node;
-//         node->prev = head;
-//     }
-// public:
-//     LRUCache(int capacity){
-//         cap = capacity;
-//         head = new Node(0,0);
-//         tail = new Node(0,0);
-//         head->next = tail;
-//         tail->prev = head;
-//     }
-
-//     int get(int key) {
-//         if(mp.find(key) == mp.end()) return -1;
-
-//         Node* node = mp[key];
-//         remove(node);
-//         insert(node);
-//         return node->val;
-
-//     }
-
-//     void put(int key, int value) 
-//     {
-//         if(mp.find(key) != mp.end()){
-//         Node* node = mp[key];
-//         remove(node);
-//         insert(node);
-//         return;
-//         }
-//     if(mp.size() == cap){
-//         Node* lru = tail->prev;
-//         mp.erase(lru->key);
-//         remove(lru);
-//         delete lru;
-//     }
-//     Node* newNode = new Node(key,value);
-//     insert(newNode);
-//     mp[key] = newNode;
-//     }
-// };
-// /**
-//  * Your LRUCache object will be instantiated and called as such:
-//  * LRUCache* obj = new LRUCache(capacity);
-//  * int param_1 = obj->get(key);
-//  * obj->put(key,value);
-//  */
 
 class LRUCache {
     int cap;
@@ -114,10 +43,16 @@ public:
     void put(int key, int value) 
     {
         if(mp.find(key) != mp.end()){
-
             remove(mp[key]);
             mp.erase(key);
         }
+        // if(mp.find(key) != mp.end()){
+        // Node* node = mp[key];
+        // node->val = value;
+        // remove(node);
+        // insert(node);
+        // return;
+        // }
     if(mp.size() == cap){
         Node* lru = tail->prev;
         mp.erase(lru->key);
