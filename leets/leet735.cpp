6ad7b98b681f9555e753810ddd48db3971b966d5
Adi-112 <adp6943@gmail.com>
2026-02-13 16:21:12 +0530

    asteroids collision

---
+++ leets/leet735.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        stack<int> st;
+        for(int a : ast)
+        {
+            bool alive = true;
+            while(alive && a < 0 && !st.empty() && st.top() > 0)
+            {
+              if(st.top() < -a){
+                st.pop();
+              }else if(st.top () == -a){
+                st.pop();
+                alive = false;
+              }
+              else alive = false;
+              
+            }
+            if(alive) st.push(a);
+        }
+        vector<int> res(st.size());
+        for(int i = st.size()-1; i >= 0; i--)
+        {
+            res[i] = st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
