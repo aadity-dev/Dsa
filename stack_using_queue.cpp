36deaa5b2cd1ac703a15ca88555d57a940c2d24f
Adi-112 <adp6943@gmail.com>
2026-02-14 22:37:58 +0530

    stack using queue

---
+++ stack_using_queue.cpp
@@ -0,0 +1,25 @@
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() { }
+    void push(int x) {
+        q.push(x);
+        for(int i = 0; i<q.size() - 1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int temp = q.front();
+        q.pop();
+        return temp;
+    }
+
+    int top() {
+        return q.front();
+    }
+    bool empty() {
+        return q.empty();
+    }
+};
