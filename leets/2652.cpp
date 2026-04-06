7fd4c5ef66bf2c367ab2fbc12b176413d00201bb
Adi-112 <adp6943@gmail.com>
2026-04-06 18:56:11 +0530

    Sum Multiples

--- leets/2652.cpp
+++ leets/2652.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int sumOfMultiples(int n) 
     {
-        vector<int> nums;
-        for(int i = 1;i<=n;i++){
-            nums.push_back(i);
-        }
         int sum = 0;
-        for(int i = 1;i<=nums.size();i++){
+        for(int i = 1;i<=n;i++){
             if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
                 sum += i;
             }
