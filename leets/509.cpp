8308bed12d92d7788c13ea832d38b60d8a8513f3
Adi-112 <adp6943@gmail.com>
2026-04-28 16:09:12 +0530

    Renamed 50.cpp to 509.cpp

---
+++ leets/509.cpp
@@ -0,0 +1,34 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//        if(n<=1){
+//         return n;
+//        }
+//        return fib(n-1) + fib(n-2);
+//     }
+// };
+// class Solution {
+// public:
+//     int fib(int n) {
+//     if(n <= 1) return n;
+//     vector<int> dp(n+1);
+//     dp[0] = 0;
+//     dp[1] = 1;
+//     for(int i = 2;i <= n; i++){
+//         dp[i] = dp[i - 2] + dp[i - 1];
+//     }
+//     return dp[n];
+//     }
+// };
+class Solution {
+public:
+    int f(int n, vector<int> &dp){
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = f(n-1, dp)+f(n-2, dp);
+    }
+    int fib(int n) {
+    vector<int>dp(n+1, -1);
+        return f(n, dp);
+    }
+};
