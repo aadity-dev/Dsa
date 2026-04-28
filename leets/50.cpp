35d87a1d0b2340b53f774de16d2086bba6b0aba4
Adi-112 <adp6943@gmail.com>
2026-04-28 15:50:02 +0530

    solved usign tabulation dp

---
+++ leets/50.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//        if(n<=1){
+//         return n;
+//        }
+//        return fib(n-1) + fib(n-2);
+//     }
+// };
+class Solution {
+public:
+    int fib(int n) {
+    if(n <= 1) return n;
+    vector<int> dp(n+1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i = 2;i <= n; i++){
+        dp[i] = dp[i - 2] + dp[i - 1];
+    }
+    return dp[n];
+    }
+};
