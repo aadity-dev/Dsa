d108f8a2a3c52fa914cebd363a8e02eba8d79c42
Adi-112 <adp6943@gmail.com>
2026-06-24 10:16:29 +0530

    Max Consecutive Ones

---
+++ prg.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // int age;
+    // int Marks;
+    // cout << "Enter age: ";
+    // cin >> age;
+    // cout << "Enter marks: ";
+    // cin >> Marks;
+    // if(age >= 18 && Marks > 10){
+    //     cout<< "Eligible for test" << endl;
+    // }
+    // else{
+    //     cout<< "Not Eligible for test" << endl;
+    // char grade;
+    // switch(grade){
+    //     case 'A':
+    //         cout<< "Excellent" << endl;
+    //         break;
+    //     case 'B':
+    //         cout<< "Good" << endl;
+    //         break;
+    //     case 'C':
+    //         cout<< "Fair" << endl;
+    //         break;      
+    //     case 'D':
+    //         cout<< "Poor" << endl;
+    //         break;
+    //     default:
+    //         cout<< "Invalid " << endl;
+    // }
+    int x = 10, y = 20;
+    ( x > y ) ? cout << "x is greater than y" << endl : cout << "y is greater than x" << endl;
+}
