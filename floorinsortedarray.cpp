77d10a889b9e6db73985f4bd4bb6383704241f56
Adi-112 <adp6943@gmail.com>
2026-06-26 11:52:33 +0530

    floor in sorted array

---
+++ floorinsortedarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, int x) {
+        // code here
+        int left = 0;
+        int right = arr.size() - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] <= x){
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
