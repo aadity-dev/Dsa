6bb6400cbf488b012d3cfcdff7a16c1149c7eee8
Adi-112 <adp6943@gmail.com>
2026-06-28 22:45:20 +0530

    rotate array opmizied code

--- leets/189.cpp
+++ leets/189.cpp
@@ -1,14 +1,24 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//        int n = nums.size();
+//        k = k % n;
+//        while(k--){
+//         int lst = nums[n-1];
+//         for(int i=n-1;i>0;i--){
+//             nums[i] = nums[i-1];
+//         }
+//         nums[0] = lst;
+//        }
+//     }
+// };
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
        int n = nums.size();
        k = k % n;
-       while(k--){
-        int lst = nums[n-1];
-        for(int i=n-1;i>0;i--){
-            nums[i] = nums[i-1];
-        }
-        nums[0] = lst;
-       }
+       reverse(nums.begin(), nums.end());
+       reverse(nums.begin(), nums.begin() + k);
+       reverse(nums.begin() + k,nums.end());
     }
 };
