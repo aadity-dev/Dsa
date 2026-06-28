0f32a697a95023a4707178a2d6ace92e46f1e1f2
Adi-112 <adp6943@gmail.com>
2026-06-28 21:59:37 +0530

    Rotate String Optimized

--- 796.cpp
+++ 796.cpp
@@ -15,3 +15,12 @@ public:
         
     }
 };
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()){
+            return false;
+        }
+        return ( s + s).find(goal) != string::npos;
+    }
+};
