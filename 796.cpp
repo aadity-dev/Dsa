94da8ade31b707d627d790ca5b7900d4bb4cb1c3
Adi-112 <adp6943@gmail.com>
2026-06-28 21:58:15 +0530

    Rotate String bruteforce

---
+++ 796.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()){
+            return false;
+        }
+        for(int i=0;i<s.size();i++){
+            if(s == goal) return true;
+            
+            char first = s[0];
+            s.erase(0,1);
+            s += first;
+        }
+        return false;
+        
+    }
+};
