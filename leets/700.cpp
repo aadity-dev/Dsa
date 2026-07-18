47573fd910b940b0a5db8dbc64b71ffe22dbc7c7
Adi-112 <adp6943@gmail.com>
2026-07-18 11:47:35 +0530

    revise Search in a Binary Search Tree

--- leets/700.cpp
+++ leets/700.cpp
@@ -24,3 +24,14 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL ||root->val == val) return root;
+        if(val <= root->val){
+            return searchBST(root->left, val);
+        }
+            return searchBST(root->right, val);
+    }
+};
